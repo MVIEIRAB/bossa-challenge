6be402e87a93c7e99f8614f48fee33b586eb20b6
matheus <mvieirinhaa@gmail.com>
2020-08-14 02:12:05 -0300

    criando arquivo onde ficarão os testes da api

---
+++ test.js
@@ -0,0 +1,30 @@
+const { stub } = require('sinon')
+const { expect } = require('chai')
+const mongooseConnection = require('./src/database/index')
+const { ToolsController } = require('./src/controller/ToolsController')
+
+describe('Tools Controller', () => {
+    describe('List all Tools', () => {
+        it('should return all tools', async () => {
+
+        })
+    })
+
+    describe('QUERY search by TAGS', () => {
+        it('should return all tools what have a same VALUE', async () => {
+
+        })
+    })
+
+    describe('Create one Tool', () => {
+        it('should create a tool', async () => {
+
+        })
+    })
+
+    describe('Delete one Tool', () => {
+        it('should delete one tool by ID', async () => {
+
+        })
+    })
+})
