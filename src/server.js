8ebf295fe5d4b5310fee44e18c095eef2f9d9af4
matheus <mvieirinhaa@gmail.com>
2020-08-14 02:12:31 -0300

    orgizando codigo

--- src/server.js
+++ src/server.js
@@ -5,12 +5,11 @@ const router = require('./routes.js')
 
 const app = express()
 
+const port = 3000
+
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/', router)
 
-app.listen(3000, () => {
-    console.log('server has been iniciated.')
-    console.log('CTRL + C: to stop the server.')
-})
+app.listen(port, () => console.log(`server has been iniciated and listening on port: ${port}`))
