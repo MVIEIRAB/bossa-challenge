a6d5ec53f646213ba295e97ed912dbb452572710
matheus <mvieirinhaa@gmail.com>
2020-08-16 20:43:46 -0300

    alterando variavel de ambiente

--- src/database/index.js
+++ src/database/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
 
-mongoose.connect(process.env.DB_URL, {
+mongoose.connect(process.env.URL_MONGO, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
