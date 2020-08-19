1f2df2681b6fdcd1523cc822105e89060be811ff
matheus <mvieirinhaa@gmail.com>
2020-08-18 22:07:40 -0300

    apagando arquivos desnecessarios

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: false }))
 
 app.use('/', router)
 
